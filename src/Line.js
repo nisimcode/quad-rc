6d2f7f75b9ffd6fdc57d01ec3d3de4bf6e0aac1b
nisimgit <nisimlevi@ymail.com>
2022-05-28 01:08:16 +0300

    ...

--- src/Line.js
+++ src/Line.js
@@ -159,19 +159,19 @@ export default function Line (props) {
       />
 
       <Button variant="outline-success" disabled={props.tries !== props.pr}
-              style={{marginLeft: 5, marginBottom: '1%', width: '8%', fontSize: 18, textAlign: "center"}}
+              style={{marginLeft: 5, marginBottom: '1%', width: '8%', fontSize: 18, textAlign: "left"}}
               onClick={checkLetters}>
         GO
       </Button>
 
       <Button variant="outline-warning" disabled={props.tries !== props.pr}
-              style={{marginLeft: 5, marginBottom: '1%', width: '8%', fontSize: 8, textAlign: "center"}}
+              style={{marginLeft: 5, marginBottom: '1%', width: '8%', fontSize: 10, textAlign: "left"}}
               onClick={() => {setPlc1(""); setPlc2(""); setPlc3(""); setPlc4(""); setPlc5("")}}>
         Clear
       </Button>
 
       <Button variant="outline-danger" disabled={props.tries !== props.pr}
-              style={{marginLeft: 5, marginBottom: '1%', width: '8%', fontSize: 8, textAlign: "center"}}
+              style={{marginLeft: 5, marginBottom: '1%', width: '8%', fontSize: 10, textAlign: "left"}}
               onClick={() => props.checkWin(false)}>
         Pass
       </Button>
