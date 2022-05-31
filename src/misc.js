44521f3cc804382ce62261628e0aa665067e7013
nisimgit <nisimlevi@ymail.com>
2022-05-31 22:13:55 +0300

    ...

--- src/misc.js
+++ src/misc.js
@@ -1,5 +1,5 @@
-export const BACK_URL = "http://127.0.0.1:8000"
-// export const BACK_URL = "https://wordwhile-d.herokuapp.com"
+// export const BACK_URL = "http://127.0.0.1:8000"
+export const BACK_URL = "https://wordwhile-d.herokuapp.com"
 
 export const NOTES_URL = BACK_URL + "/notes"
 export const VOCAB_URL = BACK_URL + "/vocabulary"
